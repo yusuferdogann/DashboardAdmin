24d4fa26300b9e6b683e177666cce1549ffd5904
yusuferdogann <erdogan1251@gmail.com>
2024-11-04 16:24:19 +0300

    change mongodb setings

--- server/config/database.js
+++ server/config/database.js
@@ -6,11 +6,8 @@ const database = () =>{
  
 
     
-        mongoose.connect(process.env.MONGO_URI,{
-            // useNewUrlParser : true,
-            useUnifiedTopology: true
-            
-        }).then(()=>{
+        mongoose.connect(process.env.MONGO_URI,{})
+        .then(()=>{
             console.log("mongooDB connected");
         }).catch((err)=>{
             console.log(err);
