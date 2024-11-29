aa15890828793fa673768b25aac2c67e6526d620
yusuferdogann <erdogan1251@gmail.com>
2024-11-29 18:41:09 +0300

    vite.config dosyasi guncellendi

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [vercel()],
+});
