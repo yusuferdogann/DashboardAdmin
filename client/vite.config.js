331cb9d7bbe2bc4f2660f3d09b62f176b73ca863
yusuferdogann <erdogan1251@gmail.com>
2024-11-29 19:25:16 +0300

    verce.json sayfasi rewrites yerine routes olarak sayfa guncellendi

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT as unknown as number,
-  },
-  plugins: [vercel()],
-});
+  plugins: [react()],
+})
