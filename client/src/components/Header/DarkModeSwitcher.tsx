27542ee60cd6bfdcbc54faf425682de31ae811f0
yusuferdogann <erdogan1251@gmail.com>
2025-02-04 12:43:45 +0300

    excel indirilirken loading duzenlemesi yapildi

--- client/src/components/Header/DarkModeSwitcher.tsx
+++ client/src/components/Header/DarkModeSwitcher.tsx
@@ -20,7 +20,8 @@ const DarkModeSwitcher = () => {
           className="dur absolute top-0 z-50 m-0 h-full w-full cursor-pointer opacity-0"
         />
         <span
-          className={`absolute top-1/2 left-[3px] flex h-6 w-6 -translate-y-1/2 translate-x-0 items-center justify-center rounded-full bg-white shadow-switcher duration-75 ease-linear ${
+        style={{top:'86%'}}
+          className={`absolute  left-[3px] flex h-6 w-6 -translate-y-1/2 translate-x-0 items-center justify-center rounded-full bg-white shadow-switcher duration-75 ease-linear ${
             colorMode === 'dark' && '!right-[3px] !translate-x-full'
           }`}
         >
