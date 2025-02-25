1be6b2c7d9c26d570ed6dc7f232c51a890010681
yusuferdogann <erdogan1251@gmail.com>
2025-02-25 21:19:40 +0300

    deneme 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    include: ["react-country-state-dropdown"],
+    include: ['react-country-state-dropdown']
   },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["html2canvas"],
-    },
-  },
-});
+})
