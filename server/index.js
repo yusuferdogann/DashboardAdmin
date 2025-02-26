2d9aa966bb20e91be90ee09bf9e96c36bc3a29b9
yusuferdogann <erdogan1251@gmail.com>
2025-02-26 23:22:45 +0300

    server update

--- server/index.js
+++ server/index.js
@@ -1,83 +1,62 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const database = require("./config/database.js");
-const bodyParser = require("body-parser");
-const authRouter = require("./routes/AuthRouter.js")
-const ProductRouter = require("./routes/ProductRouter.js")
-const routers = require('./routes/AuthRouter.js');
-var httpContext = require('express-http-context');
+const authRouter = require("./routes/AuthRouter.js");
+const productRouter = require("./routes/ProductRouter.js");
+// const mainRouter = require("./routes/MainRouter.js"); // Ana router dosyan
+const httpContext = require("express-http-context");
 const customErrorHandler = require("./Middleware/errors/customErrorHandler.js");
 const cookieParser = require("cookie-parser");
 
-
-
-
-
-const app = express();
-app.use(cors());  
-app.use(bodyParser.json());  
-app.use(express.json());  // JSON middleware'i en son eklensin!
-app.use(bodyParser.urlencoded({ extended: true }));  
 dotenv.config();
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+const app = express();
+
+// CORS Middleware (Tüm HTTP metotlarına izin verildi)
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+
     next();
-})
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+});
 
-app.use(express.json({ limit: '50mb' }));
+// JSON ve URL-encoded verileri kabul et (body-parser yerine express.json kullan)
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+// Cookie Parser
+app.use(cookieParser());
 
-
-app.use('/api',routers)
-
+// HTTP Context Middleware
 app.use(httpContext.middleware);
 
+// Ana Router Yapısı
+// app.use("/api", mainRouter);
+app.use("/auth", authRouter);
+app.use("/products", productRouter);
 
+// Test Route
+app.get("/yusuf", (req, res) => {
+    res.send("selam");
+});
 
-
-
-
-const PORT =  3000;
-
-
-app.get("/yusuf",(req,res)=>{
-    res.send("selam")
-})
-
-
-
-
-
-
-app.use("/auth",authRouter)
-app.use("/products",ProductRouter)
-
-
-
-
+// Veritabanı Bağlantısı
 database();
 
 // Error Handler
-app.use(customErrorHandler)
-// Static Files
-// app.use(express.static(path.join(__dirname,'public')));
-// app.use(express.static(__dirname +'/'));
+app.use(customErrorHandler);
 
+// Server Başlatma
+// const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
 
-
-//app.listen(()=>{
-//    console.log("server is running", 3001);
-//})
-app.listen(3000,()=>{
-    console.log("server is running");
-})
-
-
-module.export = app;
+module.exports = app;
